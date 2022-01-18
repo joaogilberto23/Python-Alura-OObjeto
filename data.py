6fd5c449b9fe89d6bf667e4e27df1aec1034f770
João Gilberto <joaogilberto_ns@yahoo.com.br>
2022-01-18 10:41:01 -0300

    Implementando métodos

---
+++ data.py
@@ -0,0 +1,9 @@
+class Data:
+
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    def dataFormatada(self):
+        print(f"{self.dia}/{self.mes}/{self.ano}")
--- testes.py
+++ testes.py
@@ -1,4 +1,5 @@
 from conta import Conta
+from data import  Data
 
 conta = Conta(123, "João Gilberto", 55.0, 1000.0)
 conta2 = Conta(456, "Patrícia Andrade", 100.0, 2000.0)
@@ -9,10 +10,17 @@ print(f"\nA conta de número {conta.numero}, do titular {conta.titular}"
 
 print(f"\nA conta de número {conta2.numero}, do titular {conta2.titular}"
       f" tem um saldo de {conta2.saldo} e um limite de {conta2.limite}.")
-"""
+
 
 conta.extrato()
 conta.deposito(100)
 conta.extrato()
 conta.saque(5)
 conta.extrato()
+
+"""
+
+data = Data('21', '11', '2007')
+data.dataFormatada()
+
+
