a8ae2a6cc2ab3c4995b2e7df639ba14cf7cd9205
João Gilberto <joaogilberto_ns@yahoo.com.br>
2022-01-20 21:58:10 -0300

    Herança, generalização e especialização

--- Parte1/testes.py
+++ Parte1/testes.py
@@ -7,14 +7,13 @@ from data import Data
 conta = Conta(123, "João Gilberto", 55.0, 1000.0)
 conta2 = Conta(456, "Patrícia Andrade", 100.0, 1000.0)
 
-"""
 print(f"A conta de número {conta.saldo}, do titular {conta.titular}"
       f" tem um saldo de {conta.saldo} e um limite de {conta.limite}.")
 
 print(f"A conta de número {conta2.numero}, do titular {conta2.titular}"
       f" tem um saldo de {conta2.saldo} e um limite de {conta2.limite}.")
 
-OBS: Não é possível acessar mais os dados acima após deixar os atributos privados.
+# OBS: Não é possível acessar mais os dados acima após deixar os atributos privados.
 
 # Testando os métodos da Classe Conta
 
@@ -69,11 +68,3 @@ print(conta.codigos_bancos())
 
 codigos = conta.codigos_bancos()
 print(codigos['Caixa'])
-
-"""
-
-
-
-
-
-
