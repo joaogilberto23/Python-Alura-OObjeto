26e7f3885b14ffe4799bed140598d3e271d226fc
João Gilberto <joaogilberto_ns@yahoo.com.br>
2022-01-21 15:24:35 -0300

    Polimorfismo

---
+++ Parte2/3polimorfismo.py
@@ -0,0 +1,57 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    # Implemantando uma forma de retornar o conteúdo da classe em texto/string
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    # Implemantando uma forma de retornar o conteúdo da classe em texto/string
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+    # Implemantando uma forma de retornar o conteúdo da classe em texto/string
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+listinha = [atlanta, vingadores]
+
+# Testando o retorno da classe em formato de texto
+for programa in listinha:
+    print(programa)
